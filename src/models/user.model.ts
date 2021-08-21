0ff97496359389e008988461033a1e2e487c1010
Santiago Guzman Martinez <santiguzguz0104@gmail.com>
2021-08-21 10:11:21 -0500

    Despliegue

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -15,6 +15,10 @@ const user = new Schema({
         type: String,
         required: true
     },
+    role: {
+        type: String,
+        required: true
+    },
     email:{
         type: String,
         required: true,
