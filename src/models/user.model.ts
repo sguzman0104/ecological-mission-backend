e79bad671ffc17dea1e7a4e74209137284723fbd
Santiago Guzman Martinez <santiguzguz0104@gmail.com>
2021-08-23 06:44:05 -0500

    User model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -17,7 +17,8 @@ const user = new Schema({
     },
     role: {
         type: String,
-        required: true
+        required: true,
+        default: 'user'
     },
     email:{
         type: String,
